610bff8c7f5b12e98f8eec13ad5f4da74ec7ba8e
James Huang <jhuang2003@yahoo.ca>
2022-04-21 18:30:11 -0700

    fixed styling for code input

--- src/Components/RoomCodeInput.tsx
+++ src/Components/RoomCodeInput.tsx
@@ -1,4 +1,6 @@
 import {FC, useRef} from 'react';
+import {motion} from 'framer-motion';
+import { button_hover } from '../motion-variants';
 
 const RoomCodeInput:FC = () => {
     const codeRef = useRef<HTMLInputElement | null>(null);
@@ -12,8 +14,12 @@ const RoomCodeInput:FC = () => {
     }
     return (
         <form onSubmit = {formSubmit} className = 'code-input-form'>
-            <input ref = {codeRef} type="text" name="room code"/>
-            <button type = 'submit'>Chat!</button>
+            <input ref = {codeRef} type="text" placeholder = 'Room Code' name="room code"/>
+            <motion.button variants = {button_hover} 
+                animate = 'initial' 
+                whileHover = 'hover' 
+                whileFocus = 'hover' 
+                whileTap = 'click' type = 'submit' className = 'button'>Chat!</motion.button>
         </form>
     )
 }
--- src/motion-variants.ts
+++ src/motion-variants.ts
@@ -27,13 +27,14 @@ const button_hover = {
 const page_transition = {
     initial: {
         x: '-100vw',
-        opacity: 0
+        opacity: 0,
     },
     animate: {
         //x: 0,
         x:'0',
         opacity: 1,
         transition: {
+            delay: 0.2,
             duration: 0.2,
             type: 'spring', 
             damping: 25,
